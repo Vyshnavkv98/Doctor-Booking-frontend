c8fb2338d9aada3a52e1854a897dd7a26aa0fd1c
vyshnavkv <vyshnavkv89@gmail.com>
2024-02-03 12:05:36 +0530

    changes in landing page

--- src/components/loginLanding/LoginLanding.jsx
+++ src/components/loginLanding/LoginLanding.jsx
@@ -13,7 +13,7 @@ function LoginLanding() {
         <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 fixed w-full">
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="#" class="flex items-center">
-                    <img src="#" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                    <img src="#" class="mr-3 h-6 sm:h-9" alt=" Logo" />
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Meds</span>
                 </a>
                 <div class="flex items-center lg:order-2">
